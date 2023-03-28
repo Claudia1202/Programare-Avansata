4ede5c9348ab8393d3620546e92af5464b7bda67
Claudia1202 <38378137+Claudia1202@users.noreply.github.com>
2023-03-28 22:39:40 +0300

    Update MainFrame.java

--- Compulsory 6/lab6/src/main/java/org/example/MainFrame.java
+++ Compulsory 6/lab6/src/main/java/org/example/MainFrame.java
@@ -16,18 +16,14 @@ public class MainFrame extends JFrame {
     private void init() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        //create the components
         configPanel = new ConfigPanel(this);
         canvas = new DrawingPanel(this);
         controlPanel = new ControlPanel(this);
 
-        //arrange the components in the container (frame)
-        //JFrame uses a BorderLayout by default
         add(configPanel, BorderLayout.NORTH);
         add(canvas, BorderLayout.CENTER);
         add(controlPanel, BorderLayout.SOUTH);
 
-        //invoke the layout manager
         pack();
     }
 }
