3b5a6da575c084d651a01ac6bc197e2429a4474f
Claudia1202 <38378137+Claudia1202@users.noreply.github.com>
2023-04-27 21:37:10 +0300

    Add files via upload

---
+++ Compulsory 8/lab8/src/main/java/org/example/App.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String args[]) {
+        try {
+            var artists = new ArtistDAO();
+            artists.create("Pink Floyd");
+            artists.create("Michael Jackson");
+            System.out.println("Artist id:" + artists.findByName("Pink Floyd"));
+            Database.getConnection().commit();
+            var genres = new GenreDAO();
+            genres.create("Rock");
+            genres.create("Pop");
+            System.out.println("Genre id:" + genres.findByName("Rock"));
+            var albums = new AlbumDAO();
+            albums.create(1979, "The Wall", "Pink Floyd", "Rock");
+            albums.PrintAlbum();
+
+            Database.getConnection().commit();
+
+            Database.getConnection().close();
+        } catch (SQLException e) {
+            System.err.println(e);
+            Database.rollback();
+        }
+    }
+}
